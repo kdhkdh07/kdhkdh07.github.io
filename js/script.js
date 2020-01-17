31422b2fb3a462a57b331c2e1ae004c9fcd05f5b
kdhkdh07 <donghyun960702@gmail.com>
2020-01-18 01:47:57 +0900

    main logo downward instagram icon bottom logo added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function(){
     var scrolling;
 
 
-    // Under bar
+    // bottom bar
 
     $(window).load(function(){
       $(window).scrollTop(1);
@@ -18,8 +18,9 @@ jQuery(document).ready(function(){
       $(".pro_bar").width(nowHeight / scrollHeight * $(window).width());
       clearInterval(scrolling);
 
-      var height = $(document).scrollTop(); //실시간으로 스크롤의 높이를 측정
-      if(height > 167){
+      var height = $(document).scrollTop(); //스크롤 높이 측
+
+      if(height > 167){ //하단 메뉴 바 fix 상태 바꾸기
         $('.btm_menu').addClass('fixfix');
         console.log("!");
       }else if(height <= 167){
@@ -30,19 +31,5 @@ jQuery(document).ready(function(){
     if ($(document).height() == $(window).height()) {
       $(".pro_bar").css("width", "100%");
     }
-
-    // $.fn.scrollEnd = function(callback, timeout) {
-    //   $(window).scroll(function(){
-    //     clearInterval(scrolling);
-    //     var $this = $(window);
-    //     if ($this.data('scrollTimeout')) {
-    //       clearTimeout($this.data('scrollTimeout'));
-    //     }
-    //     $this.data('scrollTimeout', setTimeout(callback, timeout));
-    //   });
-    // };
-    //
-
-
 
   });
